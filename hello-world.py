0b3ada70134f9fdcd86c5c321e0a99f2e50302ce
aguetler26 <aguetler26@icapital.at>
2018-04-16 16:27:37 +0200

    noch mehr eingaben...

--- hello-world.py
+++ hello-world.py
@@ -1,3 +1,7 @@
 # Hello World mit python
 name = input("What's your name?")
 print ("Hello World!", name)
+
+alter = input("How old are you?")
+print ("This year you are ", alter)
+print ("Next year you are ", int(alter)+1)
